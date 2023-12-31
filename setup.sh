86ef8b0433acee1287d957018eaf9424d9639470
VDawg <vdawg@tuta.io>
2023-12-31 03:06:50 +0100

    split and merge

---
+++ setup.sh
@@ -0,0 +1,19 @@
+hyprctl keyword decoration:blur:enabled false >/dev/null
+hyprctl keyword decoration:rounding 0 >/dev/null
+hyprctl keyword decoration:drop_shadow false >/dev/null
+
+hyprctl keyword general:gaps_in 5 >/dev/null
+hyprctl keyword general:gaps_out 48 >/dev/null
+hyprctl keyword general:gaps_workspaces 0 >/dev/null
+
+hyprctl keyword general:border_size 0 >/dev/null
+
+hyprctl keyword general:resize_on_border false >/dev/null
+hyprctl keyword animation windowsMove, 1, 0.6666666, linear 
+
+# for i in {1..63}; do
+#     kitty >/dev/null & disown kitty
+# done
+
+
+echo hiii
